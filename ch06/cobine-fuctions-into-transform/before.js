d5f608bc2d29309fce99f8e9b191037ae223fcf1
maronnjapan <tihoutaikai20@gmail.com>
2024-05-27 12:03:11 +0900

    関数群の集約への変換のサンプル実装

--- ch06/cobine-fuctions-into-transform/before.js
+++ ch06/cobine-fuctions-into-transform/before.js
@@ -1,22 +1,12 @@
-const acquireReading = () => ({
-  customer: "ivan",
-  quantity: 10,
-  month: 5,
-  year: 2017,
-});
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
+const acuquireReading = () => reading;
+const aReading = acuquireReading();
 
-// client1 最初は基本料金を出すために記載していた
-const aReading = acquireReading();
 const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
 
-// client2 課税対象料金を計算する処理にも使われている
-const aReading2 = acquireReading();
-const base = baseRate(aReading2.month, aReading2.year) * aReading2.quantity;
-const taxableCharge = Math.max(0, base - taxThreshold(aReading2.year));
+const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
 
-// client3 ここでも同じような処理が使われている
-const aReading3 = acquireReading();
-const basicChargeAmount =
-  baseRate(aReading3.month, aReading3.year) * aReading3.quantity;
+const basicChargeAmount = calculateBaseCharge(aReading);
 const calculateBaseCharge = (aReading) =>
   baseRate(aReading.month, aReading.year) * aReading.quantity;
--- ch06/cobine-fuctions-into-transform/finish.js
+++ ch06/cobine-fuctions-into-transform/finish.js
@@ -1,15 +1,11 @@
-const acquireReading = () => ({
-  customer: "ivan",
-  quantity: 10,
-  month: 5,
-  year: 2017,
-});
-
-const calculateBaseCharge = (aReading) =>
-  baseRate(aReading.month, aReading.year) * aReading.quantity;
+const reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
+const acquireReading = () => reading;
 
 const enrichReading = (original) => {
   const result = _.cloneDeep(original);
+  const calculateBaseCharge = (aReading) =>
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+
   result.baseCharge = calculateBaseCharge(result);
   result.taxableCharge = Math.max(
     0,
