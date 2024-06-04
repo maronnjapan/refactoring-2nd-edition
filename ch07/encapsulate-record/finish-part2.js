f70956bb462a1edb41ca39c30eeb3df1a234d9f4
maronnjapan <tihoutaikai20@gmail.com>
2024-06-04 12:25:07 +0900

    サンプルコードpart2のプッシュ

--- ch07/encapsulate-record/finish-part2.js
+++ ch07/encapsulate-record/finish-part2.js
@@ -1,4 +1,4 @@
-const customerData = {
+const customerData2 = {
   1920: {
     name: "martin",
     id: "1920",
@@ -15,10 +15,45 @@ const customerData = {
   },
 };
 
-customerData[customerID].usages[year][month] = amount;
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+
+  setUsage(customerID, year, month, amount) {
+    return (this._data[customerID].usages[year][month] = amount);
+  }
+
+  get rawData() {
+    return _.cloneDeep(this._data);
+  }
+
+  usage(customerID, year, month) {
+    return this._data[customerID].usages[year][month];
+  }
+}
+let customerData = new CustomerData({
+  1920: {
+    name: "martin",
+    id: "1920",
+    usages: {
+      2016: {
+        1: 50,
+        2: 55,
+      },
+      2015: {
+        1: 70,
+        2: 63,
+      },
+    },
+  },
+});
+const getCustomerData = () => customerData;
+
+getCustomerData().setUsage(customerID, year, month, amount);
 
 const compareUsage = (customerID, lateYear, month) => {
-  const later = customerData[customerID].usages[lateYear][month];
-  const earlier = customerData[customerID].usages[lateYear - 1][month];
+  const later = getCustomerData().usage(customerID, lateYear, month);
+  const earlier = getCustomerData().usage(customerID, lateYear - 1, month);
   return { laterAmount: later, change: later - earlier };
 };
